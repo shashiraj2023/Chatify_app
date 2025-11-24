44c4ada6378f477706d2abcb248b52ead33d75f7
shashiraj2023 <shashirajpatna2004@gmail.com>
2025-11-25 00:12:45 +0530

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,6 +55,9 @@ export const signup = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+  if(!email || !password){
+    return res.status(400).json({ message: "Email and password are required" });
+  }
 
   try {
     // FIXED: add await
